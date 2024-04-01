7d3f7645ae9499b3bfb45b8475f490bf5c57d13a
Uedson Reis <uedson.reis@sovos.com>
2024-04-01 10:53:59 -0300

    WIP

--- angular_app/src/app/app.component.ts
+++ angular_app/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular_app';
-  label = 'Vamos programar?';
+    title = 'angular_app';
+    label = 'Exemplo de Microfrontend';
 }
